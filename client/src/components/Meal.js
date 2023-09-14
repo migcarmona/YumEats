9526b48ad5f32b97c74d3cd488894b0a1f60aab8
migcarmona <migcarmona@gmail.com>
2023-09-14 07:43:31 +0100

    14-09-2023 meal

--- client/src/components/Meal.js
+++ client/src/components/Meal.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {mealData} from '../data/data'
 
 const Meal = () => {
   return (
     <div className='max-w-[1520px] m-auto px-4 py-12'>
         <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Our Meal</h1>
-        <div className='grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-4'></div>
+        <div className='grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+          {
+            mealData.map((item)=>(
+              <div className='border-none hover:scale-105 duration-300'>
+                <img alt={item.name} key={item.id} src={item.image} className='w-full h-[200px] object-cover rounded-lg'/>
+                <div className='flex justify-between py-2 px-4'>
+                  <p className=' bg-orange-700 h-18 w-18 rounded-full -mt-10 p-2 text-white border-8'>{item.price}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
     </div>
   )
 }
